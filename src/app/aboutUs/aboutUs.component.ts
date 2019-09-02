a37d390620bf6b637b4c1270c4b1e500ff9e2af1
Abhishek Parmar <abhishek67parmar>
2019-09-02 18:55:29 +0530

    Added About Us Page

--- src/app/aboutUs/aboutUs.component.ts
+++ src/app/aboutUs/aboutUs.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-aboutUs',
@@ -10,6 +11,22 @@ export class AboutUsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $(document).ready(function() {
+      let w = $('.eimg').width();
+      $('.eimg').css({ 'height': w });
+
+
+      let h = $('.eimg').height();
+      let he = $('.e-name').height();
+      let hd = $('.d-name').height();
+
+      let total = h + he + hd + 40;
+
+      $('.flip-card-inner').css({ 'height': total });
+      $('.flip-card-front').css({ 'height': total });
+      $('.flip-card-back').css({ 'height': total });
+      $('.flip-card').css({ 'height': total });
+    });
   }
 
 }
