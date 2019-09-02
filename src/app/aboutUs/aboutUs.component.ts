12978e3d149a8c9939df74f48f658f04da7feb46
Abhishek Parmar <abhishek67parmar>
2019-09-02 19:48:55 +0530

    img rounded updated

--- src/app/aboutUs/aboutUs.component.ts
+++ src/app/aboutUs/aboutUs.component.ts
@@ -12,20 +12,25 @@ export class AboutUsComponent implements OnInit {
 
   ngOnInit() {
     $(document).ready(function() {
-      // let w = $('.eimg').width();
-      // $('.eimg').css({ 'height': w });
-
+      let w = $('.eimg').width();
+      $('.eimg').css({ 'height': w });
 
       let h = $('.eimg').height();
       let he = $('.e-name').height();
       let hd = $('.d-name').height();
 
-      let total = h + he + hd + 40;
+      let total = h + he + hd + 50;
 
       $('.flip-card-inner').css({ 'height': total });
       $('.flip-card-front').css({ 'height': total });
       $('.flip-card-back').css({ 'height': total });
       $('.flip-card').css({ 'height': total });
+
+      console.log($('.names').height());
+      if($('.names').height()> 32){
+        console.log("test");
+        $('.img-row').css({ 'margin-top': '-25px' });
+      }
     });
   }
 
