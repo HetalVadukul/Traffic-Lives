b891d4458824de169255cc9936bf6000377b8182
Abhishek Parmar <abhishek67parmar>
2019-09-08 00:40:04 +0530

    change in validation

--- src/app/submitUs/submitUs.component.ts
+++ src/app/submitUs/submitUs.component.ts
@@ -13,37 +13,40 @@ export class SubmitUsComponent implements OnInit {
 
   constructor(private fb: FormBuilder) { }
   len: number;
-  val: any = Validators.maxLength(60);
+  vall: any;
 
   onChange(category) {
     console.log(category);
 
     if (category === 'ref') {
       this.len = 280;
-      this.val = Validators.maxLength(280);
+      this.vall = Validators.maxLength(280);
+      this.submitus.controls['story'].setValidators([this.vall]);
     } else if (category === 'nf') {
       this.len = 60;
-      this.val = Validators.maxLength(60);
+      this.vall = Validators.maxLength(60);
+      this.submitus.controls['story'].setValidators([this.vall]);
     } else if (category === 'mt') {
       this.len = 180;
-      this.val = Validators.maxLength(180);
+      this.vall = Validators.maxLength(180);
+      this.submitus.controls['story'].setValidators([this.vall]);
     }
 }
 
   ngOnInit() {
 
     // this.categoryLengthValidation = Validators.maxLength(180);
-    this.len = 180;
+    this.len = 180; 
 
-    // let val = Validators.maxLength(60);
+    this.vall = Validators.maxLength(180);
 
     this.submitus = this.fb.group({
-      fName: ['', [Validators.minLength(2), Validators.pattern('^[a-zA-Z ]+$')]],
-      lName: ['', [Validators.minLength(2), Validators.pattern('^[a-zA-Z ]+$')]],
+      fName: ['', [Validators.required, Validators.minLength(2), Validators.pattern('^[a-zA-Z ]+$')]],
+      lName: ['', [Validators.required, Validators.minLength(2), Validators.pattern('^[a-zA-Z ]+$')]],
       email: ['', [Validators.required, Validators.email]],
       instaId: [''],
       category: ['mt'],
-      story: ['', [this.val]]
+      story: ['', [Validators.required, this.vall]]
     });
   }
 
