b50e62c9d98686a3ed168300f5c71ea9823f26bf
Abhishek Parmar <abhishek67parmar>
2019-09-10 02:10:23 +0530

    added story page

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  contentful: {
+    spaceId: 'osihrc81d502',
+    token: 'KcveT4EW2KcP3zO8TXbu-ybknmxB5H1qplY14k3AO8w'
+  }
 };
